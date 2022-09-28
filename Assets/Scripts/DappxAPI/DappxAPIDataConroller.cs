d2974847dcaec218c29ebb6d9fdd52fe5502e530
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-09-29 02:08:37 +0900

    ConncetDappXApi
    ConnectDappXApi
    PlayerName = usernae
    Add Wallet {coinStorage balance : zera, ace, dappx}

--- Assets/Scripts/DappxAPI/DappxAPIDataConroller.cs
+++ Assets/Scripts/DappxAPI/DappxAPIDataConroller.cs
@@ -17,6 +17,7 @@ public class DappxAPIDataConroller : MonoBehaviour
     [SerializeField] string FullAppsProductionURL = "https://odin-api.browseosiris.com";
     [SerializeField] string FullAppsStagingURL = "https://odin-api-sat.browseosiris.com";
 
+
     #region Don't_Destroy_Awake
     private void Awake()
     {
@@ -48,40 +49,237 @@ public class DappxAPIDataConroller : MonoBehaviour
 
     #region Variables
     // Json 형태의 유저 정보를 담을 Class.
-    GetUserProfile getUserProfile = null;
+    GetUserProfile  getUserProfile      = null;
+    GetSessionID    getSessionID        = null;
+    BetSettings     betSettings         = null;
+    BalanceInfo     zeraBalanceInfo     = null;     // Save CoinStorageInfo 
+    BalanceInfo     aceBalanceInfo      = null;     // Save CoinStorageInfo 
+    BalanceInfo     dappXBalanceInfo    = null;     // Save CoinStorageInfo 
+
+    public GetUserProfile   GetUserProfile      { get { return getUserProfile; }}
+    public GetSessionID     GetSessionID        { get { return getSessionID; }}
+    public BetSettings      BetSettings         { get { return betSettings; }}
+    public BalanceInfo      ZeraBalanceInfo     { get { return zeraBalanceInfo; }}
+    public BalanceInfo      AceBalanceInfo      { get { return aceBalanceInfo; }}
+    public BalanceInfo      DappXBalanceInfo    { get { return dappXBalanceInfo; }}
     #endregion
 
-    public void OnClick_StartUserSetting()
-    {
-        StartCoroutine(ProcessRequestGetUserInfo());
-    }
 
+    #region Function_for_Connectting_UI
+    public void OnClick_StartUserSetting() => StartCoroutine(ProcessRequestGetUserInfo());
+
+    // zera 잔고 확인
+    public void Check_ZeraCoinBalance() => StartCoroutine(ProcessRequestCoinBalance("zera"));
+    public void Check_AceCoinBalance() => StartCoroutine(ProcessRequestCoinBalance("ace"));
+    public void Check_DappXCoinBalance() => StartCoroutine(ProcessRequestCoinBalance("dappx"));
+    #endregion
+
+
+    // Get UserProfile Info to DappxAPI
     IEnumerator ProcessRequestGetUserInfo()
     {
         yield return RequestGetUserInfo((response) =>
         {
             if (response != null)
             {
-                Debug.Log("## " + response.ToString());
                 getUserProfile = response;
-                Debug.Log("StatusCode" + getUserProfile.StatusCode);
+                Debug.Log("## " + getUserProfile.ToString());
+                // UserInfo
+                StartCoroutine(ProcessRequestGetSessionID());
             }
-
         });
     }
 
+    // Get SessionID Info to DappxAPI
+    IEnumerator ProcessRequestGetSessionID()
+    {
+        yield return RequestGetSessionID((response) => {
+            if (response != null)
+            {
+                getSessionID = response;
+                Debug.Log("## GetSessionID : " + getSessionID.ToString());
+                StartCoroutine(ProcessRequestBetSettings());
+            }
+            
+        });
+    }
 
+    // Bring BettSetting Info to DappxAPI Until Now 
+    IEnumerator ProcessRequestBetSettings()
+    {
+        yield return RequestBetSettings((response) => {
+            if (response != null)
+            {
+                betSettings = response;
+                Debug.Log("## BetSettings " + betSettings.ToString());
+                Debug.Log("## BetSettings.settings : " + betSettings.data.settings.ToString());
+                Debug.Log("## BetSettings.bets : " + betSettings.data.bets.ToString());
+                Check_ZeraCoinBalance();
+                Check_AceCoinBalance();
+                Check_DappXCoinBalance();
+            }
+        });
+    }
+
+    // Check amount coin to Ace, Zera, Dappx to DappxAPI
+    IEnumerator ProcessRequestCoinBalance(string coinStorage)
+    {
+        
+        yield return RequestCoinBalance(coinStorage, getSessionID.sessionId, (response) =>
+        {
+            if (response != null)
+            {
+                if (coinStorage == "zera") zeraBalanceInfo = response;
+                else if (coinStorage == "ace") aceBalanceInfo = response;
+                else dappXBalanceInfo = response;
+
+                Debug.Log("## Response " + coinStorage + " Balance : " + response.ToString());
+            }
+        });
+    }
+
+    //----------------------------------------------------------
+
+    #region LocalHostApi
+    /// <summary>
+    /// Bring UserProfile to DappxAPI
+    /// </summary>
     IEnumerator RequestGetUserInfo(Action<GetUserProfile> callback)
     {
         // 유저 프로필을 가져온다.
         // UnityWebRequest : 웹 서버와 통신을 제공한다.
-        UnityWebRequest www = UnityWebRequest.Get(" http://localhost:8546/api/getuserprofile");
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8546/api/getuserprofile");
         // SendWebRequest : 원격서버와 통신을 시작하는 함수이다.
         yield return www.SendWebRequest();
-        Debug.Log(www.downloadHandler.text);
+
         //FromJson : Json의 Array형태의 값을 GetUserProfile으로 반환해준다.
         // downloadHandler 서버의 API JSON 정보를 다운로드 한다.
         GetUserProfile getUserProfile = JsonUtility.FromJson<GetUserProfile>(www.downloadHandler.text);
         callback(getUserProfile);
+        Debug.Log(getUserProfile.ToString());
+        //Debug.Log("StatusCode" + getUserProfile.StatusCode);
     }
+
+    /// <summary>
+    /// Bring SessionID to DappxAPI
+    /// </summary>
+    IEnumerator RequestGetSessionID(Action<GetSessionID> callback)
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8546/api/getsessionid");
+        yield return www.SendWebRequest();
+        GetSessionID getSessionID = JsonUtility.FromJson<GetSessionID>(www.downloadHandler.text);
+        callback(getSessionID);
+
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Bring BettSetting Info to DappxAPI
+    /// </summary>
+    IEnumerator RequestBetSettings(Action<BetSettings> callback)
+    {
+        string url = GetBaseURL() + "/v1/betting/settings";
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        www.SetRequestHeader("api-key", API_KEY);
+        yield return www.SendWebRequest();
+        BetSettings settings = JsonUtility.FromJson<BetSettings>(www.downloadHandler.text);
+        callback(settings);
+    }
+
+    // ???? 문서에는 apiKey 필요없다고 했는데..
+    /// <summary>
+    /// Check amount coin to Ace, Zera, Dappx to DappxAPI
+    /// </summary>
+    IEnumerator RequestCoinBalance(string coinStorage, string sessionID, Action<BalanceInfo> callback)
+    {
+        string url = GetBaseURL() + "/v1/betting/" + coinStorage + "/balance/" + sessionID;
+        Debug.Log(url);
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        www.SetRequestHeader("api-key", API_KEY);
+        yield return www.SendWebRequest();
+        Debug.Log(www.downloadHandler.text);
+        BalanceInfo balanceInfo = JsonUtility.FromJson<BalanceInfo>(www.downloadHandler.text);
+        callback(balanceInfo);
+    }
+
+    #region BettingCoroutin
+    /// <summary>
+    /// Bet Coin
+    /// </summary>
+    IEnumerator RequestCoinPlaceBet(string coinStorage, RequestBettingPlcaeBet request, Action<ResponseBettingPlaceBet> callback)
+    {
+        // 코인배팅 코루틴
+
+        string url = GetBaseURL() + "/v1/betting/" + coinStorage + "/place-bet";
+
+        // ??? 이거 왜하는 거지
+        string reqJsonData = JsonUtility.ToJson(request);
+        Debug.Log(reqJsonData);
+
+        UnityWebRequest www = UnityWebRequest.Post(url, reqJsonData);
+        // Post는 웹에 생성 요청을 하기 때문에 UTF8로 Json값을 인코딩해주어야 한다.
+        byte[] buff = System.Text.Encoding.UTF8.GetBytes(reqJsonData);
+        // ???? 이건 무엇?
+        www.uploadHandler = new UploadHandlerRaw(buff);
+        www.SetRequestHeader("api-key", API_KEY);
+        // ????? 문서 헤더에는 없는데...?
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.SendWebRequest();
+
+        ResponseBettingPlaceBet responseBettingPlaceBet = JsonUtility.FromJson<ResponseBettingPlaceBet>(www.downloadHandler.text);
+        callback(responseBettingPlaceBet);
+
+    }
+
+    /// <summary>
+    /// Coin Declare Winner
+    /// </summary>
+    IEnumerator RequestCoinDeclareWinner(string coinStorage, RequestBettingDeclareWinner request, Action<ResponseBettingDeclareWinner> callback)
+    {
+        // 승자 코인 회수 코루틴
+
+        string url = GetBaseURL() + "/v1/betting/" + coinStorage + "/declare-winner";
+
+        string reqJsonData = JsonUtility.ToJson(request);
+        Debug.Log(reqJsonData);
+
+        UnityWebRequest www = UnityWebRequest.Post(url, reqJsonData);
+        byte[] buff = System.Text.Encoding.UTF8.GetBytes(reqJsonData);
+        www.uploadHandler = new UploadHandlerRaw(buff);
+
+        www.SetRequestHeader("api-key", API_KEY);
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.SendWebRequest();
+
+        ResponseBettingDeclareWinner responseBettingDeclareWinner = JsonUtility.FromJson<ResponseBettingDeclareWinner>(www.downloadHandler.text);
+        callback(responseBettingDeclareWinner);
+
+    }
+
+    /// <summary>
+    /// Coin DisConnect
+    /// </summary>
+    IEnumerator RequestCoinDisConnect(string coinStorage, RequestBettingDisconnect request, Action<ResponseBettingDisconnect> callback)
+    {
+        // 배팅괸 코인을 반환하는 코루틴.
+
+        string url = GetBaseURL() + "/v1/betting/" + coinStorage + "/disconnect";
+
+        string reqJsonData = JsonUtility.ToJson(request);
+        Debug.Log(reqJsonData);
+
+        UnityWebRequest www = UnityWebRequest.Post(url, reqJsonData);
+        byte[] buff = System.Text.Encoding.UTF8.GetBytes(reqJsonData);
+        www.uploadHandler = new UploadHandlerRaw(buff);
+
+        www.SetRequestHeader("api-key", API_KEY);
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.SendWebRequest();
+
+        ResponseBettingDisconnect responseBettingDisconnect = JsonUtility.FromJson<ResponseBettingDisconnect>(www.downloadHandler.text);
+        callback(responseBettingDisconnect);
+
+    }
+    #endregion
 }
--- Assets/Scripts/DappxAPI/DappxAPIDataStructs.cs
+++ Assets/Scripts/DappxAPI/DappxAPIDataStructs.cs
@@ -1,13 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class DappxAPIDataStructs : MonoBehaviour
-{
-
-}
 /// <summary>
-/// DappX의 유저 정보를 가져온다.
+/// Bring UserProfile to DappxAPI
 /// </summary>
 [System.Serializable]
 public class GetUserProfile
@@ -28,4 +21,175 @@ public class GetUserProfile
         public string upline;
     }
     public UserProfile userProfile;
+
+    public override string ToString()
+    {
+        return $"Status :{Status} StatusCode :{StatusCode} Message :{Message} UserName : {userProfile.username}";
+    }
+}
+
+/// <summary>
+/// Bring SessionID to DappxAPI
+/// </summary>
+[System.Serializable]
+public class GetSessionID
+{
+    public string Status;
+    public string StatusCode;
+    public string Message;
+    public string sessionId;
+
+    public override string ToString()
+    {
+        return $"Status :{Status} StatusCode :{StatusCode} Message :{Message} sessionId : {sessionId}";
+    }
+}
+
+/// <summary>
+/// Bring BettSetting Info Until Now to DappxAPI
+/// </summary>
+[System.Serializable]
+public class BetSettings
+{
+    public string message;
+
+    [System.Serializable]
+    public class Settings
+    { 
+        public string _id;
+        public string game_id;
+        public bool betting;
+        public bool maintenance;
+        public string createdAt;
+        public string updatedAt;
+        public int __v;
+        public override string ToString()
+        {
+            return $"_id: {_id} game_id: {game_id} betting: {betting} maintenance: {maintenance}  createdAt: {createdAt}  updatedAt: {updatedAt}  __v: {__v} ";
+        }
+    }
+
+    [System.Serializable]
+    public class Bets
+    { 
+        public string _id;
+        public string game_id;
+        public int amount;
+        public int platform_fee;
+        public int developer_fee;
+        public int win_reward;
+        public int win_amount;
+        public string createdAt;
+        public string updatedAt;
+        public int __v;
+        public override string ToString()
+        {
+            return $"_id: {_id} game_id: {game_id} amount: {amount} platform_fee: {platform_fee}  developer_fee: {developer_fee}  win_reward: {win_reward}  win_amount: {win_amount} createdAt: {createdAt} updatedAt: {updatedAt} __v: {__v} ";
+        }
+    }
+
+    [System.Serializable]
+    public class Data
+    {
+        public Settings settings;
+        public Bets[] bets;
+    }
+
+    public Data data;
+    public override string ToString()
+    {
+        return $"message : {message}";
+    }
+}
+
+/// <summary>
+/// Check amount coin to Ace, Zera, Dappx to DappxAPI
+/// </summary>
+[System.Serializable]
+public class BalanceInfo
+{
+    public string message;
+
+    [System.Serializable]
+    public class Data
+    {
+        public int balance;
+    }
+
+    public Data data;
+
+    public override string ToString()
+    {
+        return $"message:{message} Balance : {data.balance}";
+    }
+}
+
+
+// Request Place Bet
+public class RequestBettingPlcaeBet
+{
+    public string[] player_session_id;
+    public string bet_id;
+}
+/// <summary>
+/// Bet Coin
+/// </summary>
+[System.Serializable]
+public class ResponseBettingPlaceBet
+{
+    // 코인배팅
+    public string message;
+
+    [System.Serializable]
+    public class Data
+    {
+        public string betting_id;
+    }
+    public Data data;
+}
+
+// Request Declare Winner
+public class RequestBettingDeclareWinner
+{
+    public string betting_id;
+    public string winner_player_id;
+    public object match_details;
+}
+
+/// <summary>
+/// Declare Winner
+/// </summary>
+[System.Serializable]
+public class ResponseBettingDeclareWinner
+{
+    // 배팅 코인을 획득하게 될 승자.
+    public string message;
+
+    [System.Serializable]
+    public class Data
+    {
+        public int amount_won;
+    }
+    public Data data;
+}
+
+// Request Disconnect
+public class RequestBettingDisconnect
+{
+    public string betting_id;
+}
+/// <summary>
+/// Disconnect
+/// </summary>
+[System.Serializable]
+public class ResponseBettingDisconnect
+{
+    // 배팅된 코인 반환
+    public string message;
+
+    [System.Serializable]
+    public class Data
+    { 
+    }
+    public Data data;
 }
